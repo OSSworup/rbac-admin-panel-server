29c039a2b92811cd280bef84c82be9a625e7c91e
Omm Subham Sworup Ojha <ossworupojha@gmail.com>
2026-06-09 20:03:49 +0530

    allow multiple origins in cors configuration

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ const app=express();
 
 app.use(express.json());
 app.use(cors({
-    origin:"http://localhost:5173"
+    origin:["http://localhost:5173","https://rbac-admin-panel-client.vercel.app"],
 }))
 
 app.get('/',(req,res)=>{
